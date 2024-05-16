d9af575bc168435194ecff7adb6cdec8ca641fcb
John Magbual <jmagbual@Johns-Air.lan>
2024-05-15 21:02:20 -0700

    update route to add product

--- server/index.js
+++ server/index.js
@@ -1,103 +1,98 @@
-const { client, createTables, createUser, createFavorites, createUserFavorites, fetchFavorites, fetchUserFavorites, deleteUserFavorites } = require('./db');
 const express = require('express');
 const app = express();
+const { createTables, createUser, createProduct, createFavorite, fetchUsers, fetchProducts, fetchFavorites, destroyFavorite, createUserFavorites, fetchUserFavorites, deleteUserFavorites } = require('./db');
 
 app.get('/api/users', async (req, res, next) => {
-    try {
-      const users = await fetchUsers();
-      res.send(users);
-    } catch (ex) {
-      next(ex);
-    }
-  });
-  
-  app.get('/api/products', async (req, res, next) => {
-    try {
-      // Assuming you have a function fetchProducts() to retrieve products
-      const products = await fetchProducts();
-      res.send(products);
-    } catch (ex) {
-      next(ex);
-    }
-  });
-  
-  app.get('/api/users/:id/favorites', async (req, res, next) => {
-    try {
-      // Assuming you have a function fetchFavoritesByUserId(userId) to retrieve favorites by user ID
-      const favorites = await fetchFavoritesByUserId(req.params.id);
-      res.send(favorites);
-    } catch (ex) {
-      next(ex);
-    }
-  });
-
-  app.get('/api/users/:id/userFavorites', async(req, res, next)=> {
-    try {
-      res.send(await fetchUserFavorites(req.params.id));
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-
-  app.post('/api/users/:id/userFavorites', async(req, res, next)=> {
-    try {
-      res.status(201).send(await createUserFavorites({ user_id: req.params.id, favorites_id: req.body.favorites_id}));
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-
-  app.delete('/api/users/:userId/userFavorites/:id', async(req, res, next)=> {
-    try {
-      await deleteUserFavorites({ id: req.params.id, user_id: req.params.userId });
-      res.sendStatus(204);
-    }
-    catch(ex){
-      next(ex);
-    }
-  });
-  
-  
-  const init = async()=> {
-    
-    await client.connect();
-    console.log('connected to database');
-    await createTables();
-    
-    console.log('tables created');
-    
-    const [moe, lucy, ethyl, singing, dancing, juggling, plateSpinning] = await Promise.all([
-      createUser({ username: 'moe', password: 's3cr3t' }),
-      createUser({ username: 'lucy', password: 's3cr3t!!' }),
-      createUser({ username: 'ethyl', password: 'shhh' }),
-      createFavorites({ name: 'singing'}),
-      createFavorites({ name: 'dancing'}),
-      createFavorites({ name: 'juggling'}),
-      createFavorites({ name: 'plate spinning'}),
-    ]);
-    
+  try {
     const users = await fetchUsers();
-    console.log(users);
-  
-    const Favorites = await fetchFavorites();
-    console.log(Favorites);
-  
-    const userFavorites = await Promise.all([
-      createUserFavorites({ user_id: moe.id, favorites_id: plateSpinning.id}),
-      createUserFavorites({ user_id: moe.id, favorites_id: juggling.id}),
-      createUserFavorites({ user_id: ethyl.id, favorites_id: juggling.id}),
-      createUserFavorites({ user_id: lucy.id, favorites_id: dancing.id}),
-    ]);
-  
-    console.log(await fetchUserFavorites(moe.id));
-    await deleteUserFavorites(userFavorites[0].id);
-    console.log (await fetchUserFavorites(moe.id));
-  };
+    res.send(users);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
-  const port = process.env.PORT || 3000;
-  app.listen(port, ()=> console.log(`listening on port ${port}`));
+app.get('/api/products', async (req, res, next) => {
+  try {
+    const products = await fetchProducts();
+    res.send(products);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
-  
-  init();
+app.get('/api/users/:id/favorites', async (req, res, next) => {
+  try {
+    const favorites = await fetchFavorites(req.params.id);
+    res.send(favorites);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get('/api/users/:id/userFavorites', async (req, res, next) => {
+  try {
+    const userFavorites = await fetchUserFavorites(req.params.id);
+    res.send(userFavorites);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post('/api/users/:id/userFavorites', async (req, res, next) => {
+  try {
+    const newFavorite = await createUserFavorites({ user_id: req.params.id, favorites_id: req.body.favorites_id });
+    res.status(201).send(newFavorite);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete('/api/users/:userId/userFavorites/:id', async (req, res, next) => {
+  try {
+    await deleteUserFavorites({ id: req.params.id, user_id: req.params.userId });
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+const init = async () => {
+  await createTables();
+  console.log('Tables created');
+
+  const [moe, lucy, ethyl, singing, dancing, juggling, plateSpinning] = await Promise.all([
+    createUser({ username: 'moe', password: 's3cr3t' }),
+    createUser({ username: 'lucy', password: 's3cr3t!!' }),
+    createUser({ username: 'ethyl', password: 'shhh' }),
+    createProduct({ name: 'singing' }),
+    createProduct({ name: 'dancing' }),
+    createProduct({ name: 'juggling' }),
+    createProduct({ name: 'plate spinning' }),
+  ]);
+
+  const userFavorites = await Promise.all([
+    createFavorite({ user_id: moe.id, product_id: plateSpinning.id }),
+    createFavorite({ user_id: moe.id, product_id: juggling.id }),
+    createFavorite({ user_id: ethyl.id, product_id: juggling.id }),
+    createFavorite({ user_id: lucy.id, product_id: dancing.id }),
+  ]);
+
+  console.log('User favorites created:', userFavorites);
+
+  const users = await fetchUsers();
+  console.log('Users:', users);
+
+  const favorites = await fetchFavorites();
+  console.log('Favorites:', favorites);
+
+  const userFavoritesData = await fetchUserFavorites(moe.id);
+  console.log('User favorites:', userFavoritesData);
+
+  await deleteUserFavorites(userFavorites[0].id);
+  console.log('User favorites after deletion:', await fetchUserFavorites(moe.id));
+};
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+init();
