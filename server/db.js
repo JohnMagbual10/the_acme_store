02bab0e1eb75e7109d92242c8f2cbf316fa4a3d6
John Magbual <jmagbual@Johns-Air.lan>
2024-05-15 19:49:22 -0700

    tables created

--- server/db.js
+++ server/db.js
@@ -5,23 +5,23 @@ const bcrypt = require('bcrypt');
 
 const createTables = async()=> {
   const SQL = `
-    DROP TABLE IF EXISTS user_skills;
+    DROP TABLE IF EXISTS products;
     DROP TABLE IF EXISTS users;
-    DROP TABLE IF EXISTS skills;
+    DROP TABLE IF EXISTS favorites;
     CREATE TABLE users(
       id UUID PRIMARY KEY,
       username VARCHAR(20) NOT NULL UNIQUE,
       password VARCHAR(255) NOT NULL
     );
-    CREATE TABLE skills(
+    CREATE TABLE favorites(
       id UUID PRIMARY KEY,
       name VARCHAR(100) NOT NULL UNIQUE
     );
-    CREATE TABLE user_skills(
+    CREATE TABLE user_favorites(
       id UUID PRIMARY KEY,
       user_id UUID REFERENCES users(id) NOT NULL,
-      skill_id UUID REFERENCES skills(id) NOT NULL,
-      CONSTRAINT unique_user_id_skill_id UNIQUE (user_id, skill_id)
+      skill_id UUID REFERENCES favorites(id) NOT NULL,
+      CONSTRAINT unique_user_id_favorites_id UNIQUE (user_id, favorite_id)
     );
   `;
   await client.query(SQL);
